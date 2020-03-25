dfab88bf94a9dca5ebac61d65052d7da377d2552
Gleb015 <56198854+Gleb015@users.noreply.github.com>
2020-03-25 21:47:25 +0300

    Initial commit

---
+++ app/src/main/java/megarec/voice/app/SettingsActivity.java
@@ -0,0 +1,37 @@
+package megarec.voice.app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+//public
+//extends android.app.ActionBar
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
+        setSupportActionBar(toolbar);
+
+
+        ActionBar actionBar = getSupportActionBar();
+
+
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.action_settings);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new SettingsFragment())
+                .commit();
+    }
+}
